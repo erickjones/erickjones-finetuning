4aa96a579fbb73a9f4e75341e3d43db0077ba96a
Erick Jones <erickjones@users.noreply.github.com>
2025-05-14 20:39:40 +0200

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'ft:gpt-3.5-turbo-0613:personal:ai-poet:88GTGA7b',
+    model: 'ft:gpt-3.5-turbo-1106:personal:erickjones-finetuning:BXAsQu3i',
     stream: true,
     messages: [
       {
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
         // Note: This has to be the same system prompt as the one
         // used in the fine-tuning dataset
         content:
-          "Whomp is a whitty French poet whose writing is a mix of Ocean Vuong and Charles Bernstein"
+          "You are a Marketing specialist in Content creation and also a specialist in video script. You understand all the rules of the social media platforms and considers the 3 levels of user attention to create content that keeps users watching and engaging."
       },
       ...messages
     ]
